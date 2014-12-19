877169f54d90aa7f6f27b1ba861f214f56b448c4
Fabian <fabideleon01@gmail.com>
2014-12-19 11:06:09 -0200

    updated last version

--- calculatedMcWosQty.js
+++ calculatedMcWosQty.js
@@ -1,174 +1,177 @@
- var jsonArray=[];  
-var allResults = new nlapiSearchRecord(null, 235, null, null);
-    
-    for(var i = 0; i < allResults.length; i++) {
-      var json = {};
-
-      var columnsSearch = allResults[i].getAllColumns();
-      json.internalid=allResults[i].getId();
-      json.itemid = allResults[i].getValue(columnsSearch[0]); 
-      json.memberitem = allResults[i].getText(columnsSearch[1]); 
-      json.memberitem2= allResults[i].getText(columnsSearch[2]); 
-      json.itemtype = allResults[i].getRecordType();      
-      json.componentName = allResults[i].getValue(columnsSearch[5]);      
-      json.sort = allResults[i].getValue(columnsSearch[3]);      
-      json.atum = parseInt(allResults[i].getValue(columnsSearch[6]));      
-      json.atu = allResults[i].getValue(columnsSearch[7]);      
-      json.minternalid = allResults[i].getValue(columnsSearch[8]);      
-      json.mtype = allResults[i].getValue(columnsSearch[9]);      
-
-      jsonArray.push(json)
-
-    }
-
+var arrayIds = [];
+startingSeting()
 function startingSeting(){
+var actualRecordId=nlapiGetRecordId();
+nlapiLogExecution('ERROR','BEFORE','BEFORE PAUSE');
+ 
+  try{
+    var actualRecord = nlapiLoadRecord('salesorder', actualRecordId, {recordmode: 'dynamic'});
 
-try{
-       /*var actualRecordId = nlapiGetRecordId();
-
-         nlapiLogExecution('ERROR', 'sales order','sales order');
-         var actualRecord = nlapiLoadRecord('salesorder', actualRecordId,{recordmode: 'dynamic'});
-         var counterItems = actualRecord.getLineItemCount('item');
-        
-       for (var i = 1; i <= counterItems; i++) {
-
-         var typeItem = actualRecord.getLineItemValue('inventory', 'itemtype', i);  
-      
-        if(typeItem != 'EndGroup' && typeItem != 'Group'){
-         
-         var internalId = actualRecord.getLineItemValue('item', 'item', i);
-         var name = actualRecord.getLineItemText('item', 'item', i);
-         if(name != '' && name){*/
-          calculatedMcWos('','');
-        // }
-         
-       // }
-        
-     //}; 
-
-}catch(e){}
+    nlapiLogExecution('ERROR','AFTER','record charge');
     
+    var counter = actualRecord.getLineItemCount('item');
+
+    for (var i = 1; i <= counter; i++) {
+        
+        var typeItem = actualRecord.getLineItemValue('item', 'itemtype',i);
+                if (typeItem != 'Group' && typeItem != 'EndGroup') {
+
+                   
+                   var internalId = actualRecord.getLineItemValue('item', 'item', i);
+                   var itemName = actualRecord.getLineItemText('item', 'item', i);
+                   
+                   calculatedMcWos(internalId,typeItem);
+
+                   }
+    };  
+  nlapiLogExecution('ERROR', 'start : ', 'update Fields ATU');
+ 
+
+  }catch(e){
+
+    nlapiLogExecution('ERROR', 'ERROR : ',e);
+  }
+
+}
+
+
+function calculatedMcWos(internalId,typeItem){   
+  try{
+    var filter2 = [];
+    filter2[0] = new nlobjSearchFilter('item', null, 'is', internalId);
+
+      var newSearchResult = new nlapiSearchRecord(null, 234, filter2, null);
+      
+      if(newSearchResult.length>0){
+       var x=0;
+       var columnsSearch2 = newSearchResult[x].getAllColumns();
+       var qty = parseInt(newSearchResult[x].getValue(columnsSearch2[1]).replace(",",""));       
+       var itemName=newSearchResult[x].getValue(columnsSearch2[0]);       
+     }
+    
+     var type = returnTypeItem(typeItem)
+     var id = internalId;
+
+     var record = nlapiLoadRecord(type,id);
+     var counterMembers = record.getLineItemCount('member');
+     var flag = false;
+     var arrayItems = [];
+
+     for (var i = 1; i <= counterMembers && !flag; i++) {
+         
+         var memberId = record.getLineItemValue('member', 'item', i);  
+         var type = returnTypeItem(record.getLineItemValue('member', 'sitemtype', i));  
+         var fieldAtu = parseInt(nlapiLookupField(type,memberId,"custitem_available_to_use"));
+         var memberObject = {};
+         memberObject.type = type;
+         memberObject.atu = fieldAtu;
+         memberObject.id = memberId;
+
+         if(fieldAtu >= qty){
+          nlapiSubmitField(type,memberId,'custitem_pending_work_order',qty);
+          flag = true;
+          arrayIds.push(memberId)
+         }
+
+         arrayItems.push(memberObject);
+     
+     };
+     if(!flag){
+
+        var secondFlag = reviewMembers(arrayItems,qty);
+        if(!secondFlag){
+
+          for (var i = 0; i < arrayItems.length; i++) {
+             if(arrayIds.indexOf(arrayItems[i].id)== -1){
+              nlapiSubmitField(arrayItems[i].type,arrayItems[i].id,'custitem_pending_work_order',qty);
+              arrayIds.push(arrayItems[i].id)
+            }else{
+              
+              var availability = parseInt(nlapiLookupField(arrayItems[i].type,arrayItems[i].id,"custitem_pending_work_order"));
+             if(isNaN(availability))availability = 0;  
+             qty+=availability;
+              nlapiSubmitField(arrayItems[i].type,arrayItems[i].id,'custitem_pending_work_order',qty);
+            }
+             
+          };
+         
+        }
      }
 
 
-    function calculatedMcWos(internalId,itemName){   
-try{
-      var filter2 = [];
-      //filter2[0] = new nlobjSearchFilter('item', null, 'is', internalId);
 
-      var newSearchResult = new nlapiSearchRecord(null, 234, filter2, null);
-    
-      if(newSearchResult.length>0){
-           var x=0;
-           var columnsSearch2 = newSearchResult[x].getAllColumns();
-           var qty = parseInt(newSearchResult[x].getValue(columnsSearch2[1]).replace(",",""));       
-           var itemName=newSearchResult[x].getValue(columnsSearch2[0]);       
-      }else{
-          return;
-      }
-      
+}catch(e){
+  nlapiLogExecution('ERROR','type error',e)
+}
 
-      qtyleft=qty;
-      var idxMember=0;
+}
 
-      for (var i = jsonArray.length-1 ; i >=0 && qtyleft>0; i--) 
-      {
+function reviewMembers(arrayItems,qty){
 
-        if(jsonArray[xi].itemid==itemName){
+var flag = false;
 
-        for (var xi = jsonArray.length-1 ; xi >=0 && qtyleft>0; xi--) 
-        {
-      
-        if(jsonArray[xi].itemid==itemName){
-            idxMember=xi;
-          if(jsonArray[xi].atum>0){
+for (var i = 0; i < arrayItems.length && !flag; i++) {
+         
+         var record = nlapiLoadRecord(arrayItems[i].type,arrayItems[i].id);
+         var counterMembers = record.getLineItemCount('member');
 
-              var type=returnTypeItem(jsonArray[xi].mtype)
+         if(counterMembers && counterMembers >= 1){
 
-              var id=jsonArray[xi].minternalid;
+                  for (var i = 1; i <= counterMembers && !flag; i++) {
+         
+                       var memberId = record.getLineItemValue('member', 'item', i);  
+                       var type = returnTypeItem(record.getLineItemValue('member', 'sitemtype', i));  
+                       var fieldAtu = parseInt(nlapiLookupField(type,memberId,"custitem_available_to_use"));
+                       var availableMsp = parseInt(nlapiLookupField(type,memberId,"custitem_pending_work_order")); 
+                       if(isNaN(availableMsp))availableMsp = 0;
 
-              qtyleft=qty-jsonArray[xi].atum;
+                       if(arrayIds.indexOf(memberId)!=-1){
+                         nlapiSubmitField(type,memberId,'custitem_pending_work_order',(qty+availableMsp));
+                         flag = true;
+                       }else if(fieldAtu >= qty){
+                        nlapiSubmitField(type,memberId,'custitem_pending_work_order',qty);
+                        flag = true;
+                       }
+                   
+                 };
 
-              if(qtyleft<=0){ 
-                        var a=2
-                   nlapiSubmitField(type,id,'custitem_pending_work_order',qty);
-              }else{
-                    var a=4
-
-                   nlapiSubmitField(type,id,'custitem_pending_work_order',qtyleft);
-              }
-          }else{
-             itemName=jsonArray[xi].memberitem;  
-
-          }
-        }
-      }//end for
-       if(qtyleft>0){
-          itemName=jsonArray[idxMember].memberitem;  
-        }
+         }
 
 
-    }//end if
-  }//endfor
-      if(xi<0 && qtyleft>0){
-        var a=1
-         nlapiSubmitField(type,id,'custitem_pending_work_order',qtyleft);
-      }
+     
+     };
+   
+   return flag;
 
-    }catch(e){}
-      
-    }
-
-
-      /*
-
-
-
-
-
-      var filter = [];
-      filter[0] = new nlobjSearchFilter('internalid', null, 'is', internalId);
-      var searchResult = new nlapiSearchRecord(null, 109, filter, null);
-      
-      var itemAssembly = nlapiLoadRecord(searchResult[0].getRecordType(),internalId);  
-
-      try{
-      var itemQty = itemAssembly.getLineItemCount('member');
-      }catch(e){
-        var itemQty=0;
-      }
-      
-      
-
-      for (var z = 1; z <= itemQty; z++) {          
-
-        var internalidComponent = itemAssembly.getLineItemValue('member', 'item', z);
-        var typeComponent = itemAssembly.getLineItemValue('member', 'type', z);
-       
-        var nameAssembly = itemAssembly.getLineItemText('member', 'item', z);
-        var qtyNecesary = itemAssembly.getLineItemValue('member', 'quantity', z);
-        var totaltoUpdate = qtyNecesary * backOrder;
-        var typeComponentSearch = itemAssembly.getLineItemValue('member', 'sitemtype', z);
-        var typeItemComponent = returnTypeItem(typeComponentSearch);
-
-        var itemMember = nlapiLoadRecord(typeItemComponent,internalidComponent);         
-
-        if(parseFloat(totaltoUpdate) > 0){
-
-           nlapiSubmitField(itemMember.getRecordType(),itemMember.getId(),'custitem_pending_work_order',totaltoUpdate,true); 
-        }
-        */
+}
 
 
 function returnTypeItem(type){
-var retorno = '';
+  var retorno = '';
 
-if(type == 'Assembly'){
+  if(type == 'Assembly'){
 
-  retorno = 'assemblyitem';
-}else if(type == 'InvtPart'){
+    retorno = 'assemblyitem';
 
-retorno = 'inventoryitem'
+  }else if(type == 'InvtPart'){
+
+    retorno = 'inventoryitem';
+
+  }else if (type == 'Group') {
+
+    retorno = 'itemgroup';
+
+  };
+
+  return retorno;
 }
-return retorno;
-}
+
+
+function pausecomp(millis) 
+{
+var date = new Date();
+var curDate = null;
+
+do { curDate = new Date(); } 
+while(curDate-date < millis);
+} 
